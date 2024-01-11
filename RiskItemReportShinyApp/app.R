ecd633323d23cebff511b99f1218a1941171835c
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2024-01-11 15:56:10 -0600

    added schedule and performance pie charts, updated inputs to accept selectize to improve performance, added P2 input as option for reports

--- RiskItemReportShinyApp/app.R
+++ RiskItemReportShinyApp/app.R
@@ -10,6 +10,7 @@ library(DT)
 library(shinythemes)
 library(shiny)
 library(bslib)
+library(knitr)
 library(stringr)
 library(markdown)
 
@@ -25,8 +26,8 @@ erisk_ItemProj<-left_join(risk_item_db, risk_project_db, by = "PROJECT_ID")
 
 RiskImpactTable<-risk_item_db[,c("PROJECT_NAME", "RISK_NAME", "USACE_ORGANIZATION","P2_NUMBER")]
 riskpies <- erisk_ItemProj |>
-  select("PROJECT_NAME.x","RISK_NAME","RISKCATEGORY","RISK_STATEMENT", 
-         "PRIMARYMISSION", "USACE_ORGANIZATION.x", "COST_RANK_DESC", "COST_RISK_RANKING")
+  select("P2_NUMBER.x", "RISK_IDENTIFIER","PROJECT_NAME.x","RISK_NAME","RISKCATEGORY",
+         "DISCIPLINE", "USACE_ORGANIZATION.x", "COST_RANK_DESC", "SCHEDULE_RANK_DESC", "PERFORMANCE_RANK_DESC")
 
 
 
@@ -37,20 +38,20 @@ shinyApp(
                tabPanel("Project",
                         sidebarLayout(
                           sidebarPanel(
-                            selectInput("districtInput", "Select a District", choices=sort(c("",RiskImpactTable$USACE_ORGANIZATION)),selected = NULL,
+                            selectizeInput("districtInput", "Select a District", choices=sort(c("",RiskImpactTable$USACE_ORGANIZATION)),selected = NULL,
                                         multiple = F),
-                            selectInput("projectInput", "Select a project", choices=c("",RiskImpactTable$PROJECT_NAME.x)),
+                            selectizeInput("projectInput", "Select a project", choices=NULL ),
                                        h6("or"),
-                                      selectInput("P2Input","Enter a P2 Number", choices=c(" ",RiskImpactTable$P2_LOOKUP),selected = NULL,
+                                      selectizeInput("P2Input","Enter a P2 Number", choices= NULL,selected = NULL,
                                                   multiple = F
                                       ),
-                                       selectInput("riskInput", "Select a risk item", choices=sort(c(" ",RiskImpactTable$RISK_NAME)),selected = NULL,
+                                       selectizeInput("riskInput", "Select a risk item", choices= NULL ,selected = NULL,
                                                    multiple = F), 
                                        downloadButton("report", "Download report"),width=2),
                           
                           mainPanel(
                             tabsetPanel(id="Report Tabs",
-                                        tabPanel("Home - District Risk Overview", plotlyOutput("pie"),
+                                        tabPanel("Home - Project Risk Overview", plotlyOutput("pie"),
                                                  DTOutput("overviewtab")),
                                         tabPanel("Risk Item Report",
                                                  htmlOutput("reportrend")),
@@ -59,6 +60,9 @@ shinyApp(
                                         tabPanel("All Risk Items",
                                                  htmlOutput("AllRiskRend")),
                             )))))),
+  
+
+  
   server = function(input, output, session) {
     
     projects <- reactive ({RiskImpactTable |>
@@ -67,33 +71,36 @@ shinyApp(
     
     observeEvent(projects(), {
        choices <- sort(unique(projects()$PROJECT_NAME))
-      updateSelectInput(inputId = "projectInput", choices = c("",choices),
+      updateSelectizeInput(inputId = "projectInput", choices = c("",choices),
                         selected = "") 
     })
  
       P2s <- reactive({RiskImpactTable |>
-          filter(RiskImpactTable$USACE_ORGANIZATION == input$districtInput)})
+          filter(RiskImpactTable$USACE_ORGANIZATION == input$districtInput || RiskImpactTable$PROJECT_NAME == input$projectInput)})
       
       observeEvent(P2s(), {
         P2s <- sort(unique(P2s()$P2_NUMBER))
-        updateSelectInput(
+        updateSelectizeInput(
           inputId = "P2Input", 
           choices =c("",P2s), 
           selected = "")
       })
       
-    risks <-reactive({RiskImpactTable |>
-        filter(RiskImpactTable$PROJECT_NAME == input$projectInput)})
-    observeEvent(risks(),{
+      risks <-reactive({RiskImpactTable |>
+          filter(RiskImpactTable$P2_NUMBER == input$P2Input || RiskImpactTable$PROJECT_NAME == input$projectInput)})
+      observeEvent(risks(),{
         risks <- sort(unique(risks()$RISK_NAME))
-      updateSelectInput(
-        inputId = "riskInput", 
-        choices = c("",risks), selected = ""
+        updateSelectizeInput(
+          inputId = "riskInput", 
+          choices = c("",risks), selected = ""
         )
-    })
+      })
+      
+      
     
     plot<-reactive({riskpies |>
-                     filter(riskpies$USACE_ORGANIZATION.x == input$districtInput)|>
+        filter(
+          conditional(input$projectInput != "", riskpies$PROJECT_NAME.x == input$projectInput))|>
         group_by(COST_RANK_DESC) |>
         summarize(count = n())|>
         filter(COST_RANK_DESC != "No Risk")|>
@@ -104,37 +111,107 @@ shinyApp(
         plotly::arrange(factor(COST_RANK_DESC,levels=c('Opportunity', 'Low', 'Medium', 'High')))
         })
     
+    plot2<-reactive({riskpies |>
+        filter(
+          conditional(input$projectInput != "", riskpies$PROJECT_NAME.x == input$projectInput))|>
+        group_by(SCHEDULE_RANK_DESC) |>
+        summarize(count = n())|>
+        filter(SCHEDULE_RANK_DESC != "No Risk")|>
+        mutate('color' = case_when(SCHEDULE_RANK_DESC == "Opportunity" ~'rgb(31,120,180)', 
+                                   SCHEDULE_RANK_DESC=='Low'~'rgb(51,160,44)', 
+                                   SCHEDULE_RANK_DESC =='Medium'~ 'rgb(255,127,0)',
+                                   SCHEDULE_RANK_DESC == 'High'~ 'rgb(227,26,28)' ))|>
+        plotly::arrange(factor(SCHEDULE_RANK_DESC,levels=c('Opportunity', 'Low', 'Medium', 'High')))
+    })
+    
+    plot3<-reactive({riskpies |>
+        filter(
+          conditional(input$projectInput != "", riskpies$PROJECT_NAME.x == input$projectInput))|>
+        group_by(PERFORMANCE_RANK_DESC) |>
+        summarize(count = n())|>
+        filter(PERFORMANCE_RANK_DESC != "No Risk")|>
+        mutate('color' = case_when(PERFORMANCE_RANK_DESC == "Opportunity" ~'rgb(31,120,180)', 
+                                   PERFORMANCE_RANK_DESC=='Low'~'rgb(51,160,44)', 
+                                   PERFORMANCE_RANK_DESC =='Medium'~ 'rgb(255,127,0)',
+                                   PERFORMANCE_RANK_DESC == 'High'~ 'rgb(227,26,28)' ))|>
+        plotly::arrange(factor(PERFORMANCE_RANK_DESC,levels=c('Opportunity', 'Low', 'Medium', 'High')))
+    })
     
         output$pie = plotly::renderPlotly({
-          plot_ly(data = plot(),
+         fig1 <- plot_ly(textfont = list(color = '#FFFFFF')) 
+           fig1 <-fig1 |> 
+             add_pie(data = plot(),
                   values =  ~ count,
                   labels = ~ COST_RANK_DESC,
                   sort = FALSE,
                   textinfo = 'value',
+                  title = "Cost",
                   textfont = list(color = '#FFFFFF'),
                   domain = list(row = 0, column = 0),
                   marker = list(
                     colors = ~ color,
-                    line = list(color = '#FFFFFF', width = 1.5)), type="pie")
+                    line = list(color = '#FFFFFF', width = 1.5))) 
+         
+           fig1<- fig1 |>
+            add_pie(
+             data = plot2(),
+             values = ~ count,
+             labels = ~ SCHEDULE_RANK_DESC,
+             textinfo = 'value',
+             textfont = list(color = '#FFFFFF'),
+             sort = FALSE,
+             title = "Schedule",
+             domain = list(row = 0, column = 1),
+             marker = list(
+               colors = ~ color,
+               line = list(color = '#FFFFFF', width = 1.5)))
+           
+           fig1<- fig1 |>
+             add_pie(
+               data = plot3(),
+               values = ~ count,
+               labels = ~PERFORMANCE_RANK_DESC,
+               textinfo = 'value',
+               textfont = list(color = '#FFFFFF'),
+               sort = FALSE,
+               title = "Performance",
+               domain = list(row = 0, column = 2),
+               marker = list(
+                 colors = ~ color,
+                 line = list(color = '#FFFFFF', width = 1.5)))
+           
+          fig1<- fig1|>
+            layout(showlegend = T,
+                   grid=list(rows=1, columns=3),legend= list(orientation = 'h'))
+          fig1
+
           })
     
+        
+    conditional <- function(condition, success){
+      if (condition) success else TRUE}
+    
+        
     table<- reactive({riskpies |>
-        filter(riskpies$USACE_ORGANIZATION.x == input$districtInput)})
+        filter(
+          conditional(input$projectInput != "", riskpies$PROJECT_NAME.x == input$projectInput))|>
+        select(RISK_IDENTIFIER, USACE_ORGANIZATION.x, PROJECT_NAME.x, RISK_NAME, RISKCATEGORY, DISCIPLINE, COST_RANK_DESC, SCHEDULE_RANK_DESC, PERFORMANCE_RANK_DESC)
+      })
   
     output$overviewtab = DT::renderDataTable({
-      DT::datatable(table(), colnames = c("","PROJECT NAME","RISK NAME","RISKCATEGORY","RISK_STATEMENT", 
-                                          "PRIMARYMISSION", "USACE ORGANIZATION", "COST RANK DESCRIPTION", "COST RANK VALUE" ))
+      DT::datatable(table(), colnames = c("Risk Identifier","USACE Organization","Project Name","Risk Name ","Risk Category","Discipline", 
+                                          "Cost Rank", "Schedule Rank", "Performance Rank" ), rownames=FALSE, filter = "top")
     })
     
       output$reportrend <- renderUI({
         includeMarkdown(
-          rmarkdown::render("RiskItemReport.Rmd", params=list(projID = input$projectInput, riskID = input$riskInput),
+          rmarkdown::render("RiskItemReport.Rmd", params=list(projID = input$projectInput, riskID = input$riskInput, p2ID = input$P2Input),
          )
         )
       })
       output$ProjRend <- renderUI({
         includeMarkdown(
-          rmarkdown::render("ProjectAllRiskReport.Rmd", params=list(projID = input$projectInput,riskID = input$riskInput))
+          rmarkdown::render("ProjectAllRiskReport.Rmd", params=list(projID = input$projectInput,riskID = input$riskInput, p2ID = input$P2Input))
         )
       })
       output$AllRiskRend <- renderUI({
