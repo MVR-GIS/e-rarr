a327861810a225ace85d1e525c44c9ae211bbc8a
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2024-01-24 15:43:10 -0600

    created pie_prep function to take risk df and format into data for Risk pie charts

--- RiskItemReportShinyApp/R/pie_prep.R
+++ RiskItemReportShinyApp/R/pie_prep.R
@@ -1,6 +1,6 @@
-#' @title Milestone Plot
+#' @title Pie Prep
 #'
-#' @description Creates a milestone chart 
+#' @description Preps data for pie charts
 #'
 #' @param riskitem   data frame; A data frame of a filtered risk item.
 #' @param milestonedf data frame; A data frame of phase-milestone pairings
@@ -8,51 +8,47 @@
 #' @return A ggplot of current milestone on timeline of all project milestones.
 #'
 #' @examples
-#' #Get milestone data and risk item
-#' milestonedf<-read.csv("C:/workspace/e-rarr/RiskItemReportShinyApp/data/PHASEMILESTONE.csv")
-#' milestone_df<-data.frame(milestonedf)
+#' erisk_item <- read_csv("C:/workspace/e-rarr/RiskItemReportShinyApp/data/RISKLIST_FULL.csv",show_col_types = FALSE)
+#' risk_item_db<-data.frame(erisk_item)
+#' erisk_project <- read_csv("C:/workspace/e-rarr/RiskItemReportShinyApp/data/PROJECTLIST_FULL.csv",show_col_types = FALSE)
+#' risk_project_db<-data.frame(erisk_project)
+#' 
+#' 
+#'
+#' erisk_ItemProj<-left_join(risk_item_db, risk_project_db, by = "PROJECT_ID")
+#' 
+#' 
+#' RiskImpactTable<-risk_item_db[,c("PROJECT_NAME", "RISK_NAME", "USACE_ORGANIZATION","P2_NUMBER")]
+#' riskpies <- erisk_ItemProj |>
+#'   select("P2_NUMBER.x", "RISK_IDENTIFIER","PROJECT_NAME.x","RISK_NAME","RISKCATEGORY",
+#'          "DISCIPLINE", "USACE_ORGANIZATION.x", "COST_RANK_DESC", "SCHEDULE_RANK_DESC", "PERFORMANCE_RANK_DESC")
 #' 
 #' 
 #'
 #' #example
-#' milestone_plot<-milestoneplot(riskitem=risk_item, milestonedf = milestone_df)
+#' milestone_plot<-pieplot_prep(riskdf=risk_item, rankcol)
 #'
 #' @importFrom magrittr |>
 #' @importFrom dplyr mutate select arrange
 #' @importFrom rlang .data
 #' @export
 #'
-pieprep<- function(riskdf){
-cost_pie<- riskdf |>
-  group_by(COST_RANK_DESC) |>
-  summarize(count = n())|>
-  filter(COST_RANK_DESC != "No Risk") |>
-  mutate('color' = case_when(COST_RANK_DESC == "Opportunity" ~'rgb(31,120,180)', 
-                             COST_RANK_DESC=='Low'~'rgb(51,160,44)', 
-                             COST_RANK_DESC =='Medium'~ 'rgb(255,127,0)',
-                             COST_RANK_DESC == 'High'~ 'rgb(227,26,28)' ))|>
-  plotly::arrange(factor(COST_RANK_DESC,levels=c('Opportunity', 'Low', 'Medium', 'High')))
+pieprep<- function(riskdf,rankcol){
+colName<- as.name(rankcol)
+colnameq<-enquo(colName)
 
-schedule_pie<- riskdf |>
-  group_by(SCHEDULE_RANK_DESC) |>
+  pie_prepped<- riskdf |>
+  group_by(!!colnameq) |>
   summarize(count = n())|>
-  filter(SCHEDULE_RANK_DESC != "No Risk")|>
-  mutate('color' = case_when(SCHEDULE_RANK_DESC == "Opportunity" ~ 'rgb(31,120,180)', 
-                             SCHEDULE_RANK_DESC=='Low'~'rgb(51,160,44)', 
-                             SCHEDULE_RANK_DESC =='Medium'~ 'rgb(255,127,0)',
-                             SCHEDULE_RANK_DESC == 'High'~ 'rgb(227,26,28)' ))|>
-  plotly::arrange(factor(SCHEDULE_RANK_DESC,levels=c('Opportunity', 'Low', 'Medium', 'High')))
+  filter(!!colnameq != "No Risk") |>
+  mutate('color' = case_when(!!colnameq == "Opportunity" ~'rgb(31,120,180)', 
+                             !!colnameq =='Low'~'rgb(51,160,44)', 
+                             !!colnameq =='Medium'~ 'rgb(255,127,0)',
+                             !!colnameq == 'High'~ 'rgb(227,26,28)' ))|>
+  plotly::arrange(factor(!!colnameq, levels=c('Opportunity', 'Low', 'Medium', 'High')))
 
-perform_pie<- riskdf |>
-  group_by(PERFORMANCE_RANK_DESC) |>
-  summarize(count = n())|>
-  filter(PERFORMANCE_RANK_DESC != "No Risk")|>
-  mutate('color' = case_when(PERFORMANCE_RANK_DESC == "Opportunity" ~ 'rgb(31,120,180)', 
-                             PERFORMANCE_RANK_DESC=='Low'~'rgb(51,160,44)', 
-                             PERFORMANCE_RANK_DESC =='Medium'~ 'rgb(255,127,0)',
-                             PERFORMANCE_RANK_DESC == 'High'~ 'rgb(227,26,28)' ))|>
-  plotly::arrange(factor(PERFORMANCE_RANK_DESC,levels=c('Opportunity', 'Low', 'Medium', 'High')))
 
-return(cost_pie, schedule_pie,perform_pie)
+return(pie_prepped)
 
 }
+
