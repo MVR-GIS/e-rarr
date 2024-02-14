d7fd1544a6f91e705bc2eb336c5b4a1eab612699
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2024-02-14 14:26:06 -0600

    testing out function to support risk input table

---
+++ R/WrangleRiskImpact.R
@@ -0,0 +1,42 @@
+#' @title Wrangle Risk Impact
+#'
+#' @description Conditionally filters Cost and Schedule risk impacts for 
+#' reporting
+#'
+#' @param risk_item_table   data frame; A data frame of a filtered risk item.
+#' @param impacttype
+#' @return A formatted data table of risk impacts.
+#'
+#' #example
+#' milestone_plot<-pieplot_prep(riskdf=risk_item, rankcol)
+#'
+#' @importFrom dplyr mutate select arrange
+#' @importFrom rlang .data enquo
+#' @export
+#'
+impactcolumn <- function(risk_item_table, impacttype) {
+  colName <- as.name(impacttype)
+  colnameq <- rlang::enquo(colName)
+  
+
+  
+  if (paste("risk_item_table$NO_",colnameq,"_IMPACT") == 1) {
+    "No cost impact is anticipated"
+  } else if (risk_item_table$COST_IMPACT_DISTTYPE == 299) {
+    risk_item_table$COST_IMPACT_MOSTLIKELY
+  } else if (risk_item_table$COST_IMPACT_DISTTYPE == 306) {
+    c(
+      risk_item_table$COST_IMPACT_LOWEST,
+      risk_item_table$COST_IMPACT_HIGHEST,
+      risk_item_table$COST_IMPACT_EVIDENCE
+    )
+  } else if (risk_item_table$COST_IMPACT_DISTTYPE == 302) {
+    c(
+      risk_item_table$COST_IMPACT_LOWEST,
+      risk_item_table$COST_IMPACT_MOSTLIKELY,
+      risk_item_table$COST_IMPACT_HIGHEST,
+      risk_item_table$COST_IMPACT_EVIDENCE
+    )
+  }
+}
+
