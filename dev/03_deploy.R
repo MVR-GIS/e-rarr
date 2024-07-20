eb4cd8f5766e45c0099873e78cb9559abfe47f5b
Michael Dougherty <mike.p.doc.71@gmail.com>
2024-07-20 14:03:45 -0500

    added dockerfile

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -33,7 +33,7 @@ golem::add_shinyserver_file()
 
 ## Docker ----
 ## If you want to deploy via a generic Dockerfile
-golem::add_dockerfile_with_renv()
+golem::add_dockerfile(port = 3838)
 
 ## If you want to deploy to ShinyProxy
 golem::add_dockerfile_with_renv_shinyproxy()
