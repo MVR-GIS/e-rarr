695bddc127285879e7508d909d2e33d45e17ef4d
Michael <mike.p.doc.71@gmail.com>
2024-07-21 09:18:46 -0500

    resolved merge conflicts

--- R/pie_plots.R
+++ R/pie_plots.R
@@ -20,20 +20,11 @@ pie_plots<- function(cost_pie, schedule_pie, perform_pie){
       data = cost_pie,
       values =  ~ .data$count,
       labels = ~ .data$COST_RANK_DESC,
-<<<<<<< HEAD
-      title=list(text='<b> Cost <br> </b>', font = list(size = 15)),
-      name= " ",
-      textinfo='value',
-      hoverinfo = 'label+percent',
-      sort=FALSE,
-      textfont = list(color = '#FFFFFF', size=15),
-=======
       title = list(text = '<b> Cost <br> </b>', font = list(size = 15)),
       name = " ",
       textinfo = 'value',
       sort = FALSE,
       textfont = list(color = '#FFFFFF', size = 15),
->>>>>>> 093369693203a1fd0b141aa8c8c587fd08eddefd
       domain = list(row = 0, column = 0),
       marker = list(
         colors = ~ color,
@@ -45,16 +36,9 @@ pie_plots<- function(cost_pie, schedule_pie, perform_pie){
       labels = ~ .data$SCHEDULE_RANK_DESC,
       textfont = list(color = '#FFFFFF',size=15),
       sort = FALSE,
-<<<<<<< HEAD
-      name= " ",
-      textinfo='value',
-      hoverinfo = 'label+percent',
-      title =list(text='<b> Schedule <br> </b>', font = list(size = 15)),
-=======
       name = " ",
       textinfo = 'value',
       title = list(text = '<b> Schedule <br> </b>', font = list(size = 15)),
->>>>>>> 093369693203a1fd0b141aa8c8c587fd08eddefd
       domain = list(row = 0, column = 1),
       marker = list(
         colors = ~ color,
@@ -64,16 +48,9 @@ pie_plots<- function(cost_pie, schedule_pie, perform_pie){
       data = perform_pie,
       values = ~ .data$count,
       labels = ~ .data$PERFORMANCE_RANK_DESC,
-<<<<<<< HEAD
-      title = list(text='<b> Performance <br> </b>', font = list(size = 15)),
-      name= " ",
-      textinfo='value',
-      hoverinfo = 'label+percent',
-=======
       title = list(text = '<b> Performance <br> </b>', font = list(size = 15)),
       name = " ",
       textinfo = 'value',
->>>>>>> 093369693203a1fd0b141aa8c8c587fd08eddefd
       textfont = list(color = '#FFFFFF', size=15),
       sort = FALSE,
       domain = list(row = 0, column = 2),
