04845921a5319b4b6336c9afb5e4fa69eb8c2377
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2024-07-26 15:12:31 -0500

    fixed milestone plot issue

--- R/milestoneplot.R
+++ R/milestoneplot.R
@@ -30,7 +30,7 @@
 
 milestoneplot<- function(riskitem, milestonedf){
 projphases<- milestonedf |>
-  dplyr::filter(PHASEID %in% riskitem$PROJECTPHASEID)|>
+  dplyr::filter(PHASEID == riskitem$PROJECTPHASEID)|>
   dplyr::mutate('yval' = rep(0.1))
 
 mileplot <- ggplot(projphases, 
@@ -41,7 +41,7 @@ mileplot <- ggplot(projphases,
              shape = 21, stroke = .75) +
   geom_point(aes(x = riskitem$PROJECTMILESTONEID, y = 0.1), 
              colour = "#1F78B4", fill = "#1F78B4", shape = 16, size = 4) +
-  geom_text(aes(label = str_wrap(riskitem$MILESTONE, width=14)), 
+  geom_text(aes(label = str_wrap(MILESTONE, width=14)), 
             position = position_nudge(y = -0.55), size = 3) +
   scale_y_continuous(limits = c(-1, 1), expand = c(0, 0)) +
   coord_cartesian(clip = "off") +
