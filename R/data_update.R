7b4381a29360a92da2a8770174ff5f17b47ee2fd
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2024-10-08 15:45:10 -0500

    updated data_update script to use new naming conventions

--- R/data_update.R
+++ R/data_update.R
@@ -41,27 +41,27 @@ update_data <- function(key_service = "egis-db-brandonroad",
   
   # Import tables from Oracle
   erisk_project <- ROracle::dbReadTable(con_roracle, "ERR_PROJECTLIST_FULL")
-  phase_milestone <- ROracle::dbReadTable(con_roracle, "ERR_PHASEMILESTONE")
+  erisk_phase_milestone <- ROracle::dbReadTable(con_roracle, "ERR_PHASEMILESTONE")
   erisk_item <- ROracle::dbReadTable(con_roracle, "ERR_RISKLIST_FULL")
-  risk_transact <- ROracle::dbReadTable(con_roracle, "ERR_RISKTRANSACTIONLIST_FULL")
-  risk_treat <- ROracle::dbReadTable(con_roracle, "ERR_RISKTREATMENTLIST_FULL")
+  erisk_transact <- ROracle::dbReadTable(con_roracle, "ERR_RISKTRANSACTIONLIST_FULL")
+  erisk_treat <- ROracle::dbReadTable(con_roracle, "ERR_RISKTREATMENTLIST_FULL")
   erisk_orgs <- ROracle::dbReadTable(con_roracle, "ERR_USACEORGANIZATION")
   erisk_msc <- ROracle::dbReadTable(con_roracle, "ERR_TBLMSC")
   erisk_dist <- ROracle::dbReadTable(con_roracle, "ERR_TBLDISTRICT")
-  risk_modeled <- ROracle::dbReadTable(con_roracle, "ERR_RISK_MODELED")
+  erisk_modeled <- ROracle::dbReadTable(con_roracle, "ERR_RISK_MODELED")
   
   # Disconnect from the database
   ROracle::dbDisconnect(con_roracle)
   
   # Save the tables as CSV files
   readr::write_csv(erisk_project, paste0(output_path, "erisk_project.csv"))
-  readr::write_csv(phase_milestone, paste0(output_path, "phase_milestone.csv"))
+  readr::write_csv(erisk_phase_milestone, paste0(output_path, "erisk_phase_milestone.csv"))
   readr::write_csv(erisk_item, paste0(output_path, "erisk_item.csv"))
-  readr::write_csv(risk_transact, paste0(output_path, "risk_transact.csv"))
-  readr::write_csv(risk_treat, paste0(output_path, "risk_treat.csv"))
+  readr::write_csv(erisk_transact, paste0(output_path, "erisk_transact.csv"))
+  readr::write_csv(erisk_treat, paste0(output_path, "erisk_treat.csv"))
   readr::write_csv(erisk_orgs, paste0(output_path, "erisk_orgs.csv"))
   readr::write_csv(erisk_dist, paste0(output_path, "erisk_dist.csv"))
   readr::write_csv(erisk_msc, paste0(output_path, "erisk_msc.csv"))
-  readr::write_csv(risk_modeled, paste0(output_path, "erisk_modeled.csv"))
+  readr::write_csv(erisk_modeled, paste0(output_path, "erisk_modeled.csv"))
 }
 
