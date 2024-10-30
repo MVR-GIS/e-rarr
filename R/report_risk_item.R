129e3dca20d2c5608a80ac78462a6f6becac4e9c
Barrie Chileen Martinez <barrie.chileen@gmail.com>
2024-10-30 12:10:31 -0500

    updated report documentation, changed logo from hex to usace castle.

--- R/report_risk_item.R
+++ R/report_risk_item.R
@@ -10,11 +10,10 @@
 #' @param projID   character; erisk Project identifier
 #' @param p2ID     numeric; USACE P2 number
 #' 
-#' @importFrom dplyr left_join mutate select filter case_when slice_max
-#' @importFrom knitr include_graphics kable
+#' @importFrom dplyr mutate select filter left_join case_when slice_max
+#' @importFrom knitr include_graphics 
 #' @importFrom kableExtra kable_styling
 #' @importFrom formattable currency
-#' @importFrom english english
 #' @importFrom stringr str_to_title
 #' @importFrom plotly plot_ly add_pie layout 
 #' @importFrom stringr str_trim
